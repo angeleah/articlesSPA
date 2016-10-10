9a63653e4e0937270cfbd0213c96d1ec2d337abd
Angeleah Daidone <angeleah@gmail.com>
2016-10-10 14:15:21 -0500

    basic functionality to load the first set of articles

--- src/js/views/articlesCollectionView.js
+++ src/js/views/articlesCollectionView.js
@@ -1,18 +1,48 @@
 App.Views.ArticlesCollection = Backbone.View.extend({
   template: App.templates.articlesCollection,
+  articlesFetched: 0,
+  articleGroupLength: 10,
+  articlesUrl: 'http://www.stellarbiotechnologies.com/media/press-releases/json',
 
   initialize: function() {
-    console.log('inside articles collection view');
-    this.render();
+    console.log('inside application view');
+    this.loadContent();
   },
 
   render: function() {
     this.$el.html(this.template(this.templateJSON()));
   },
 
+  currentUrl: function() {
+    return this.articlesUrl + '?limit=' + this.articleGroupLength + '&offset=' + this.articlesFetched;
+  },
+
+  loadContent: function() {
+    $.ajax({
+      type: 'GET',
+      url: this.currentUrl(),
+      dataType: 'json',
+      crossDomain: true,
+    })
+      .done(_.bind(this.handleSuccessfulArticleFetch, this))
+      .fail(_.bind(this.handleUnsuccessfulArticleFetch, this));
+  },
+
+  handleSuccessfulArticleFetch: function(responseData) {
+    console.log(responseData);
+    this.newsArticles = responseData.news;
+    console.log(this.newsArticles);
+    this.render();
+  },
+
+  handleUnsuccessfulArticleFetch: function(responseData) {
+    console.log("status code" + responseData.status);
+    alert("We apologize for the inconvenience. There has been an error.(" + responseData.status+")");
+  },
+
   templateJSON: function() {
     return {
-      testValue: 'cool',
+      newsArticles: this.newsArticles,
     };
   },
 });
--- src/templates/compiled-templates/templates.js
+++ src/templates/compiled-templates/templates.js
@@ -1,9 +1,17 @@
 this["App"] = this["App"] || {};
 this["App"]["templates"] = this["App"]["templates"] || {};
-this["App"]["templates"]["articlesCollection"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    var helper;
+this["App"]["templates"]["articlesCollection"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "<div class=\"article-list\">\n  "
-    + this.escapeExpression(((helper = (helper = helpers.testValue || (depth0 != null ? depth0.testValue : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"testValue","hash":{},"data":data}) : helper)))
-    + "\n</div>\n";
+  return "    <div class=\"article-list-item\">\n      <span class=\"date\">"
+    + alias3(((helper = (helper = helpers.published || (depth0 != null ? depth0.published : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"published","hash":{},"data":data}) : helper)))
+    + "</span></br>\n      <span class=\"title\">"
+    + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</span></br>\n    </div>\n    <hr>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<div class=\"article-list\">\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.newsArticles : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "</div>\n";
 },"useData":true});
